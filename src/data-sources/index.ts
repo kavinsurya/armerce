38a6dfc04a1a24b30178ca672710fef6541036fc
kavinsuryaK <kavinsurya@solulab.com>
2022-04-25 16:59:19 +0530

    role data sources added

--- src/data-sources/index.ts
+++ src/data-sources/index.ts
@@ -1,13 +1,16 @@
 import { App } from '@core/globals'
 
 import UserDataSource from '@modules/user/data-source/user'
+import RoleDataSource from '@modules/role/data-source/role'
 
 export interface Datasources {
 	User: UserDataSource
+	Role: RoleDataSource
 }
 
 export default () => {
 	return {
 		User: new UserDataSource(App.Models.User),
+		Role: new RoleDataSource(App.Models.Role),
 	}
 }
